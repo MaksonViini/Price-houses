a1a0a92fc0a499bc6cd35f9f553872211e061590
maksonviini <maksonvinicio7@gmail.com>
2021-12-08 13:11:13 -0300

    Criando a API para servir a aplicacao web

--- App/app.py
+++ App/app.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, requests
+from starlette.requests import Request
+import pickle
 
 app = FastAPI()
 
-app.get('/')
-def hello():
-    return {"message": "Hello World"}
+
+def get_model():
+    with open('/home/maksonvinicio/Documents/GitHub/Price-houses/Models/pipe.pkl', 'rb') as f:
+        return pickle.load(f)
+
+
+@app.post('/predict')
+async def predict(request: Request):
+    data = await request.json()
+
+    # return {"dados": data.get('bedrooms')}
+    model = get_model()
+    array = [data.get('bedrooms'), data.get('bathrooms'), data.get('sqft_living'),
+             data.get('sqft_lot'), data.get('floors'), data.get('waterfront'),
+             data.get('view'),
+             data.get('condition'), data.get('grade'), data.get('sqft_above'),
+             data.get('sqft_basement'),
+             data.get('yr_built'), data.get('yr_renovated'),
+             data.get('sqft_living15'), data.get('sqft_lot15')]
+    pred = model.predict([array])
+
+    return {"Price": pred[0]}
