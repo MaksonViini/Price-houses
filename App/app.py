e04974e0d6b9bf7210ad81fc9c979051fe783779
maksonviini <maksonvinicio7@gmail.com>
2021-12-11 22:06:02 -0300

    Adicionando cors a aplicacao

--- App/app.py
+++ App/app.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 import pickle
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 def get_model():
